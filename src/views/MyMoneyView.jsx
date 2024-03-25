4dc23cbc4f776f91c5f60ba6dd5830cfe0db095a
ptn-dev <pittinan.191@gmail.com>
2024-03-26 00:11:39 +0700

    update

--- src/views/MyMoneyView.jsx
+++ src/views/MyMoneyView.jsx
@@ -8,7 +8,7 @@ import MyMoneyComponent from '../components/MyMoneyComponent'
 import FormComponent from '../components/FormComponent';
 //npm install props/types
 function MyMoneyView() {
-    
+
     const initData = [
         { id: 1, name: "ค่าผ่อนตอนโด", amount: "4500" },
         { id: 2, name: "ค่าผ่อนรถ", amount: "7300" },
@@ -17,20 +17,20 @@ function MyMoneyView() {
         { id: 5, name: "ค่าไฟบ้านอุทัย", amount: "300" },
     ]
 
-    const [items,setItem ] = useState(initData);
+    const [items, setItem] = useState(initData);
 
     const onAddNewItem = (newItem) => {
         //console.log("ข้อมูลที่ได้",newItem)
-        setItem(( preventItem) => {
-            return [ newItem,...preventItem]
+        setItem((preventItem) => { //preventItem เป็นตัวแปรมีค่าเท่ากับ value ของ useState()
+            return [newItem, ...preventItem]
         });
-        
+
     }//สร้างฟังชั่นเพื่อรอรับค่า
 
     return (
         <div>
             <TitleComponent title="แอพบัญชี รายรับ-รายจ่าย" />
-            <FormComponent onAddItem={ onAddNewItem }/>
+            <FormComponent onAddItem={onAddNewItem} />
             <MyMoneyComponent items={items} />
         </div>
     )
